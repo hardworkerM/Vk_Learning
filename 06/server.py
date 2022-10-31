e33bd3cc23cd9547a9e2570064aea0d96d086101
oodlbee <113666071+oodlbee@users.noreply.github.com>
2022-10-31 23:46:13 +0300

    Update server.py

--- 06/server.py
+++ 06/server.py
@@ -1,63 +1,77 @@
-import threading
-import requests
-import re
-import json
-import queue
 import socket
+import threading
+import queue
 import sys
-from collections import Counter
+import argparse
 
 
-def master():
-    host = socket.gethostname()
-    port = 5000
-    server = socket.socket()
-    server.bind((host, port))
-    server.listen(4)
-    conn, addr = server.accept()
-    print(f'conn={conn}, addr={addr}')
-    while True:
-        data = conn.recv(1024).decode('utf-8')
-        if not data:
-            break
-        print(f'from conn user:{data}')
+class Client:
+    def __init__(self, th_count, file: str, host=socket.gethostname(), port=5000):
+        self.file = file
+        self.th_count = th_count
+        self.host = host
+        self.port = port
+        self.que = queue.Queue(self.th_count * 2)
+        self.lock = threading.Lock()
 
-    conn.close()
+    def connect(self):
+        ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ser.connect((self.host, self.port))
+        try:
+            msg = ser.recv(1024).decode('utf-8')
+        except Exception as e:
+            print(f'Error: {str(e)}')
+            msg = 'Exit'
+            exit()
+        if msg == 'CLinet is connected!':
+            threads = [
+                threading.Thread(
+                    target=self.listen,
+                )
+                for _ in range(self.th_count)
+            ]
 
+            for th in threads:
+                th.start()
 
-def worker(url, K):
-    print('worker')
-    http = requests.get(url).text
-    word_list = re.findall('[a-zа-яё]+', http, flags=re.IGNORECASE)
-    freq = Counter(word_list).most_common(K)
-    js_freq = json.dumps(dict(freq))
+            self.queue_gen()
+
+            for th in threads:
+                th.join()
+        else:
+            exit()
+
+    def queue_gen(self):
+        with open(self.file, 'r') as f:
+            for line in f:
+                self.que.put(line)
+        for _ in range(self.th_count):
+            self.que.put(None)
+
+    def listen(self):
+        while True:
+            try:
+                ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                ser.connect((self.host, self.port))
+                msg = ser.recv(1024).decode('utf-8')
+            except ConnectionResetError:
+                break
+            if msg == 'Master':
+                url = self.que.get(timeout=1)
+                if url is None:
+                    break
+                ser.send(url.encode('utf-8'))
+            elif msg == 'Worker':
+                ser.send('Ready'.encode('utf-8'))
+                url, json_str = ser.recv(1024).decode('utf-8').split('~', 1)
+                print(f'{url}: {json_str}')
+            ser.close()
 
 
 if __name__ == '__main__':
     try:
-        w_count = sys.argv[1]
-        K = sys.argv[2]
+        num_thr = sys.argv[1]
+        file_name = sys.argv[2]
     except IndexError:
-        raise IndexError('Не все параметры были заданы при запуске')
-
-    que = queue.Queue()
-    lock = threading.Lock()
-
-    master_tr = threading.Thread(
-            target=master,
-        )
-
-    worker_tr = [
-        threading.Thread(
-            target=worker,
-            args=(que, K,),
-        )
-        for _ in range(w_count)
-    ]
-
-    master_tr.start()
-
-    for th in worker_tr:
-        th.start()
-
-
+        raise IndexError('Not all arguments are given')
+    Client(num_thr, file_name).connect()
